02aa0d2c337d0e7a3e0b95668b72a08ab26d83a0
nirojkarki1 <nirojkarki85@gmail.com>
2023-06-21 10:15:01 -0500

    OrderRequest

--- CustomerBackend/src/main/java/com/example/CustomerBackend/controller/OrderController.java
+++ CustomerBackend/src/main/java/com/example/CustomerBackend/controller/OrderController.java
@@ -1,5 +1,35 @@
 package com.example.CustomerBackend.controller;
 
-public class OrderController {
+import com.example.CustomerBackend.dto.OrderRequest;
+import com.example.CustomerBackend.enitity.Customer;
+import com.example.CustomerBackend.repository.CustomerRepository;
+import com.example.CustomerBackend.repository.DependentRepository;
+import com.example.CustomerBackend.repository.ImageRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+@RestController
+public class OrderController {
+    @Autowired
+    private CustomerRepository customerRepository;
+    @Autowired
+    private DependentRepository dependentRepository;
+
+     @Autowired
+    private ImageRepository imageRepository;
+
+    @PostMapping("/placeOrder")
+    public Customer placeOrder(@RequestBody OrderRequest request){
+        return customerRepository.save(request.getCustomer());
+    }
+
+    @GetMapping("/findAllOrders")
+    public List<Customer> findAllOrders(){
+        return customerRepository.findAll();
+    }
 }
